36bb965f2a9275feb092c86f800410a051124e14
akshayaachu5169 <akshayaachu5169@gmail.com>
2023-09-02 16:59:40 +0530

    initial commit

---
+++ C3_WebDriverTest/src/test/java/interviewMavenTests/A7_OtherImportantQuestions.java
@@ -0,0 +1,12 @@
+package interviewMavenTests;
+
+public class A7_OtherImportantQuestions {
+
+	//diff between  thread.sleep and setSpeed
+	
+	//can we open the browser without the system.setProperty
+	//Ans: yes, by adding the equivalent path
+	
+	//note: everytime when we add system.setProperty, it adds the equivalent path
+	//which can be done manually also
+}
