b43a24d6343ac108355c1b623f33984e90681f18
akshayaachu5169 <akshayaachu5169@gmail.com>
2023-09-23 21:25:50 +0530

    initial commit

---
+++ C3_WebDriverTest/src/test/java/interviewMavenTests/A8_AuthenticationAlert.java
@@ -0,0 +1,21 @@
+package interviewMavenTests;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class A8_AuthenticationAlert {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd = new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+		
+	}
+	
+	
+}
